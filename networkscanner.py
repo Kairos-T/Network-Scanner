bad5125cd868d689fde071f020c0253d7ef6bedc
Kairos Tay <80029462+Kairos-T@users.noreply.github.com>
2023-08-09 14:07:59 +0800

    Update networkscanner.py

--- networkscanner.py
+++ networkscanner.py
@@ -1,29 +1,49 @@
 import socket
-import subprocess
 import sys
 
-target = input("Enter the IP  address to scan: ")
+def is_valid_ip(ip):
+    try:
+        socket.inet_aton(ip)
+        return True
+    except socket.error:
+        return False
 
-try:
-    socket.inet_aton(target)
-except socket.error:
-    print("Invalid IP address")
-    sys.exit()
-
-port_range = range(1, 100)
-
-def scan(port): #TCP socket creation
-
+def scan_port(ip, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     s.settimeout(0.5)
+    try:
+        s.connect((ip, port))
+        s.close()  # Close the socket after successful connection
+        return True
+    except (socket.timeout, ConnectionRefusedError):
+        return False
+
+def main():
+    target = input("Enter the IP address to scan: ")
+
+    if not is_valid_ip(target):
+        print("Invalid IP address")
+        sys.exit()
 
     try:
-        s.connect((target, port))
-        return True
-    except:
-        return False
-    
-for port in port_range:
-    if scan(port):
-        print(f"Port {port} is open")
+        start_port = int(input("Enter the starting port: "))
+        end_port = int(input("Enter the ending port: "))
+    except ValueError:
+        print("Invalid port range")
+        sys.exit()
+
+    open_ports = []
+
+    for port in range(start_port, end_port + 1):
+        if scan_port(target, port):
+            open_ports.append(port)
+
+    if open_ports:
+        print("Open ports:")
+        for port in open_ports:
+            print(f"Port {port} is open")
+    else:
+        print("No open ports found in the specified range.")
+
+if __name__ == "__main__":
+    main()
